7cd361180f074603fa39173dc19500642f9c6f11
Anthony Habash <redwolf98@gmail.com>
2017-12-12 19:20:05 -0500

    New Start

---
+++ models/gift.js
@@ -0,0 +1,30 @@
+// var Sequelize = require("sequelize");
+// // sequelize (lowercase) references our connection to the DB.
+// var sequelize = require("../config/connection.js");
+module.exports = function(sequelize, DataTypes){
+
+    var Gift = sequelize.define("gift",{
+        id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+        },
+        userID:{
+            type: DataTypes.INTEGER
+        },
+        relationID:{
+            type: DataTypes.INTEGER
+        },
+        name:{
+            type: DataTypes.STRING
+        },
+        description:{
+            type: DataTypes.STRING
+        },
+        image_url:{
+            type: DataTypes.STRING
+        }
+    });
+        return Gift;
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+const ejsLayouts = require("express-ejs-layouts");
+var index = require('./routes/index')
+
+// bring in the models
+var db = require("./models");
+
+var app = express();
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(__dirname + "./public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');;
+
+app.use(ejsLayouts);
+
+
+app.use('/', index);
+// app.use("/create", routes);
+
+
+// listen on port 3000
+var port = process.env.PORT || 3000;
+db.sequelize.sync().then(function () {
+  app.listen(port, function () {
+    console.log("server listening on port", port);
+  });
+});
