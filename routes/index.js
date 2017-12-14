e435035bb352d54359f4992909f4383a41e80e5c
jwinham4740 <jwinham4740@gmail.com>
2017-12-14 18:05:24 -0500

    removed router object

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,60 @@
 var express = require('express');
-var router = express.Router();
+
 const db = require("../models");
 
 var authenticateController = require('../controllers/authenticate-controller');
 var signUpController = require('../controllers/signUp-controller');
 
-router.post('/signup', signUpController.signUp, function (req, res) {
-    console.log("app.post/signup");
-    console.log(req.body);
-    res.redirect("/home");
-});
-router.post('/login', authenticateController.authenticate, function (req, res) {
-    res.redirect("/home");
-});
 
-
-router.get('/', function (req, res, next) {
-
-    res.render('login', {
-        message: ""
+module.exports = function (app) {
+    app.post('/signup', signUpController.signUp, function (req, res) {
+        console.log("app.post/signup");
+        console.log(req.body);
+        res.redirect("/home");
     });
-});
-router.get('/signup', function (req, res) {
-    res.render("signup", {
-        message: ""
+    app.post('/login', authenticateController.authenticate, function (req, res) {
+        res.redirect("/home");
     });
-});
-router.post('/signup', function (req, res) {
-    console.log("app.post/signup");
-    console.log(req.body);
-    res.redirect("/home");
-
-});
 
 
-router.get('/home', function (req, res, next) {
-    console.log("rendering home");
-    res.render('home', {});
-});
+    app.get('/', function (req, res, next) {
 
-//return status(200) if email/password have match, status(404) if email/password doesn't exist
-router.get("/login", function (req, res) {
-    db.user.findAll({
-        where: {
-            email: req.body.email,
-            password: req.body.password
-        }
-    }).then(function (results) {
-        if (results.length == 1) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+        res.render('login', {
+            message: ""
+        });
     });
-});
+    app.get('/signup', function (req, res) {
+        res.render("signup", {
+            message: ""
+        });
+    });
+    app.post('/signup', function (req, res) {
+        console.log("app.post/signup");
+        console.log(req.body);
+        res.redirect("/home");
 
-module.exports = router;
+    });
+
+
+    app.get('/home', function (req, res, next) {
+        console.log("rendering home");
+        res.render('home', {});
+    });
+
+    //return status(200) if email/password have match, status(404) if email/password doesn't exist
+    app.get("/login", function (req, res) {
+        db.user.findAll({
+            where: {
+                email: req.body.email,
+                password: req.body.password
+            }
+        }).then(function (results) {
+            if (results.length == 1) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
+
+}
