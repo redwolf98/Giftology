c16f1212e4684b9fd6983590e065079f799ad899
Anthony Habash <redwolf98@gmail.com>
2017-12-13 19:16:34 -0500

    updates to route

---
+++ routes/user-routes.js
@@ -0,0 +1,21 @@
+var db = require("../models");
+
+modules.exports = function(app){
+
+    app.get("/user", function(req,res){
+
+    });
+
+    app.post("/user", function(req,res){
+
+    });
+
+    app.put('/user', function(req, res) {
+        
+    });
+
+    app.delete('/user', function(req, res) {
+        
+    });
+
+};
