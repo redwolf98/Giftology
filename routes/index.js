8d585ed309617e56ba8bb0f25b45db6e0ee05ba2
Margaret Rumble <megrumble@gmail.com>
2017-12-15 15:40:28 -0500

    people page

--- routes/index.js
+++ routes/index.js
@@ -88,25 +88,69 @@ module.exports = function (app) {
         console.log("rendering home");
         res.render('home', {});
     });
-    app.get('/people', function (req, res, next) {
-        console.log("rendering people");
-        res.render('people', {});
+
+    app.get("/people", function (req, res) {
+        res.render('people', {
+            message: ""
+        });
+        // db.user.findAll({
+        //     where: {
+        //         id: req.body.id
+        //     }
+        // }).then(function (data) {
+        //     res.send(data);
+        // });
     });
 
-    //return status(200) if email/password have match, status(404) if email/password doesn't exist
-    app.get("/login", function (req, res) {
-        db.user.findAll({
-            where: {
-                email: req.body.email,
-                password: req.body.password
+
+    app.post('/people', function (req, res) {
+        console.log("posting person")
+        var person = {
+            "firstName": req.body.first_name,
+            "lastName": req.body.last_name,
+            "relationhip": req.body.relationship,
+            "birthDate": req.body.birthdate,
+
+        }
+        var message;
+        let status = false;
+        connection.query('INSERT INTO relation SET ?', person, function (error, results, fields) {
+                if (error) {
+
+                    message: 'there is some error with query'
+                }
+                else {
+                    status = true;
+                    message: 'person added sucessfully'
+                };
+                if (status) {
+                    res.redirect('/people-list');
+                } else {
+                    res.render('people', {
+                        message: message
+                    })
+                }
             }
-        }).then(function (results) {
-            if (results.length == 1) {
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
-            }
-        });
+
+        );
     });
+
+
+
+    //return status(200) if email/password have match, status(404) if email/password doesn't exist
+    // app.get("/login", function (req, res) {
+    //     db.user.findAll({
+    //         where: {
+    //             email: req.body.email,
+    //             password: req.body.password
+    //         }
+    //     }).then(function (results) {
+    //         if (results.length == 1) {
+    //             return res.status(404).end();
+    //         } else {
+    //             res.status(200).end();
+    //         }
+    //     });
+    // });
 
 }
--- routes/relation-routes.js
+++ routes/relation-routes.js
@@ -2,30 +2,53 @@ var db = require("../models");
 
 module.exports = function (app) {
 
-    app.get("/people", function (req, res) {
-        res.render('people');
-        db.user.findAll({
-            where: {
-                id: req.body.id
-            }
-        }).then(function (data) {
-            res.send(data);
-        });
-    });
+    // app.get("/people", function (req, res) {
+    //     res.render('people', {
+    //         message: ""
+    //     });
+    //     // db.user.findAll({
+    //     //     where: {
+    //     //         id: req.body.id
+    //     //     }
+    //     // }).then(function (data) {
+    //     //     res.send(data);
+    //     // });
+    // });
 
-    app.post("/people", function (req, res) {
-        db.user.create({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: req.body.password,
-            email: req.body.email,
-            photo_url: req.body.photo_url
-        }).then(
-            function (data) {
-                res.status(200).send(data);
-            }
-        );
-    });
+    // app.post("/people", function (req, res) {
+    //     module.exports = function (app) {
+    //         app.post('/signup', function (req, res) {
+    //             var person = {
+    //                 "firstName": req.body.first_name,
+    //                 "lastName": req.body.last_name,
+    //                 "relationship": req.body.relationship,
+    //                 "birthDate": req.body.birthdate,
+
+    //             }
+    //             var message;
+    //             let status = false;
+    //             connection.query('INSERT INTO relation SET ?', person, function (error, results, fields) {
+    //                     if (error) {
+
+    //                         message: 'there is some error with query'
+    //                     }
+    //                     else {
+    //                         status = true;
+    //                         message: 'person added sucessfully'
+    //                     };
+    //                     if (status) {
+    //                         res.redirect('/people-list');
+    //                     } else {
+    //                         res.render('people', {
+    //                             message: message
+    //                         })
+    //                     }
+    //                 }
+
+    //             );
+    //         });
+    //     }
+    // });
 
     app.put('/people', function (req, res) {
         db.user.update({
