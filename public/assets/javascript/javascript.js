11a3cd7da095c743d3843a4735ceec31860a221f
Anthony Habash <redwolf98@gmail.com>
2017-12-15 19:18:16 -0500

    add Relations

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -48,6 +48,8 @@ $(".walmart-btn").on("click", () => {
     
 });
 
+
+
 //Walmart: Used to make AJAX calls to the Walmart Search API. Parameters: correct queryURL and a product name
 function Walmart(queryURL, product) {
     //AJAX call
